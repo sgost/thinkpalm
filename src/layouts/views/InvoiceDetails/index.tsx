0690a9cb318843f8126ace70ce2746a1991ac726
Ehsan <ehsans.contractor@elementsgs.com>
2022-03-16 18:58:57 +0500

    detail view complete

--- src/layouts/views/InvoiceDetails/index.tsx
+++ src/layouts/views/InvoiceDetails/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Icon, Table } from "atlasuikit";
 import "./invoiceDetails.scss";
-import { countrySummaryData } from "./mockData";
+import { countrySummaryData, feeSummary, payrollData } from "./mockData";
+import spainFlag from "./spainFlag.png";
 
 export default function InvoiceDetails() {
+  const [activeTab, setActiveTab] = useState("payroll");
+  const [isDownloadOpen, setIsDownloadOpen] = useState(false);
+
   return (
     <div className="invoiceDetailsContainer">
       <div className="invoiceDetailsHeaderRow">
@@ -13,7 +17,10 @@ export default function InvoiceDetails() {
           <p className="text">Payroll Invoice No. 791230</p>
         </div>
         <div className="buttons">
-          <div className="download">
+          <div
+            onClick={() => setIsDownloadOpen(!isDownloadOpen)}
+            className="download"
+          >
             <p className="text">Download</p>
             <Icon
               className="icon"
@@ -23,6 +30,14 @@ export default function InvoiceDetails() {
             />
           </div>
 
+          {isDownloadOpen && (
+            <div className="openDownloadDropdown">
+              <p>Invoice as PDF</p>
+              <p>Invoice as Excel</p>
+              <p>Employee Breakdown</p>
+            </div>
+          )}
+
           <Button
             className="primary-blue small"
             icon={{
@@ -30,7 +45,7 @@ export default function InvoiceDetails() {
               icon: "checkMark",
               size: "medium",
             }}
-            label="Primary Button"
+            label="Approve Invoice"
           />
         </div>
       </div>
@@ -94,18 +109,127 @@ export default function InvoiceDetails() {
       </div>
 
       <div className="tab">
-        <p className="tabTextPassive">Payroll Journal</p>
-        <p className="tabTextActive">Master Invoice</p>
+        <p
+          onClick={() => setActiveTab("payroll")}
+          className={
+            activeTab === "payroll" ? "tabTextActive" : "tabTextPassive"
+          }
+        >
+          Payroll Journal
+        </p>
+        <p
+          onClick={() => setActiveTab("master")}
+          className={
+            activeTab === "master" ? "tabTextActive" : "tabTextPassive"
+          }
+        >
+          Master Invoice
+        </p>
         <p className="tabTextPassive">Files & Notes</p>
       </div>
+      {activeTab === "master" ? (
+        <div>
+          <h3 className="tableHeader">Country Summary</h3>
+          <Table options={countrySummaryData} colSort />
+          <div className="countrySummaryCalc">
+            <p>Total Due</p>
+            <h3>USD 300,523.15</h3>
+          </div>
 
-      <h3>Country Summary</h3>
-      <Table options={countrySummaryData} colSort />
-      <div className="countrySummaryCalc">
-        <p>Total Due</p>
-        <h3>USD 300,523.15</h3>
-      </div>
-      <h3>Fee Summary</h3>
+          <h3 className="tableHeader">Fee Summary</h3>
+          <Table options={feeSummary} colSort />
+          <div className="feeSummaryCalc">
+            <div className="row">
+              <p className="title">Incoming Wire Payment</p>
+              <p className="amount">USD 35.00</p>
+            </div>
+            <div className="row2">
+              <p className="title">Contract Termination Fee</p>
+              <p className="amount">USD 35.00</p>
+            </div>
+            <div className="totalRow">
+              <p>Total Due</p>
+              <h3>USD 300,523.15</h3>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <div className="countryHeader">
+              <img src={spainFlag} alt="flag" />
+              <h3>Spain</h3>
+            </div>
+            <Table options={payrollData} colSort />
+            <div className="feeSummaryCalc">
+              <div className="row">
+                <p className="title">Country Subtotal Due</p>
+                <p className="amount">EUR 271,410.90</p>
+              </div>
+              <div className="row">
+                <p className="title">Country EXC Rate 0.75355</p>
+                <p className="amount">USD 121,411.98</p>
+              </div>
+              <div className="row">
+                <p className="title">In Coutry Processing Fee</p>
+                <p className="amount">USD 0.00</p>
+              </div>
+              <div className="row">
+                <p className="title">FX Bill</p>
+                <p className="amount">USD 1,821.17</p>
+              </div>
+              <div className="row2">
+                <p className="title">Total Country VAT</p>
+                <p className="amount">USD 0.00</p>
+              </div>
+              <div className="totalRow">
+                <p>Country Total Due</p>
+                <h3>USD 271,410.90</h3>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="countryHeader">
+              <img src={spainFlag} alt="flag" />
+              <h3>Spain</h3>
+            </div>
+            <Table options={payrollData} colSort />
+            <div className="feeSummaryCalc">
+              <div className="row">
+                <p className="title">Country Subtotal Due</p>
+                <p className="amount">EUR 271,410.90</p>
+              </div>
+              <div className="row">
+                <p className="title">Country EXC Rate 0.75355</p>
+                <p className="amount">USD 121,411.98</p>
+              </div>
+              <div className="row">
+                <p className="title">In Coutry Processing Fee</p>
+                <p className="amount">USD 0.00</p>
+              </div>
+              <div className="row">
+                <p className="title">FX Bill</p>
+                <p className="amount">USD 1,821.17</p>
+              </div>
+              <div className="row2">
+                <p className="title">Total Country VAT</p>
+                <p className="amount">USD 0.00</p>
+              </div>
+              <div className="totalRow">
+                <p>Country Total Due</p>
+                <h3>USD 271,410.90</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="totalContainer">
+            <div>
+              <p>Total</p>
+              <h3>USD 300,523.15</h3>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/layouts/views/InvoiceDetails/mockData.js
+++ src/layouts/views/InvoiceDetails/mockData.js
@@ -1,4 +1,5 @@
 import spainFlag from "./spainFlag.png";
+import user from "./user.png";
 
 export const countrySummaryData = {
   columns: [
@@ -114,6 +115,224 @@ export const countrySummaryData = {
       countryVAT: "0.00",
       exchangeRate: "0.75355",
       totalInUSD: "121,411.97",
+    },
+  ],
+};
+
+export const feeSummary = {
+  columns: [
+    {
+      header: "Country",
+      isDefault: true,
+      key: "country",
+    },
+    {
+      header: "Currency",
+      isDefault: true,
+      key: "currency",
+    },
+    {
+      header: "Admin Fees",
+      isDefault: true,
+      key: "adminFees",
+    },
+    {
+      header: "On/Offboardings",
+      isDefault: true,
+      key: "OnOffboardings",
+    },
+    {
+      header: "FX Rate in %",
+      isDefault: true,
+      key: "fxRate",
+    },
+    {
+      header: "FX Bill",
+      isDefault: true,
+      key: "fxBill",
+    },
+    {
+      header: "Benefits",
+      isDefault: true,
+      key: "benefits",
+    },
+    {
+      header: "Employee Contribution",
+      isDefault: true,
+      key: "employeeContribution",
+    },
+    {
+      header: "Total in USD",
+      isDefault: true,
+      key: "totalinUSD",
+    },
+  ],
+  showDefaultColumn: true,
+  data: [
+    {
+      country: {
+        value: "Spain",
+        img: { src: spainFlag },
+      },
+      currency: "EUR",
+      adminFees: "3,900.00",
+      OnOffboardings: "0.00",
+      fxRate: "1,5",
+      fxBill: "95,000.00",
+      benefits: "3,780.00",
+      employeeContribution: "0.0",
+      totalinUSD: "121,411.97",
+    },
+    {
+      country: {
+        value: "Spain",
+        img: { src: spainFlag },
+      },
+      currency: "EUR",
+      adminFees: "3,900.00",
+      OnOffboardings: "0.00",
+      fxRate: "1,5",
+      fxBill: "95,000.00",
+      benefits: "3,780.00",
+      employeeContribution: "0.0",
+      totalinUSD: "121,411.97",
+    },
+    {
+      country: {
+        value: "Spain",
+        img: { src: spainFlag },
+      },
+      currency: "EUR",
+      adminFees: "3,900.00",
+      OnOffboardings: "0.00",
+      fxRate: "1,5",
+      fxBill: "95,000.00",
+      benefits: "3,780.00",
+      employeeContribution: "0.0",
+      totalinUSD: "121,411.97",
+    },
+    {
+      country: {
+        value: "Spain",
+        img: { src: spainFlag },
+      },
+      currency: "EUR",
+      adminFees: "3,900.00",
+      OnOffboardings: "0.00",
+      fxRate: "1,5",
+      fxBill: "95,000.00",
+      benefits: "3,780.00",
+      employeeContribution: "0.0",
+      totalinUSD: "121,411.97",
+    },
+  ],
+};
+
+export const payrollData = {
+  columns: [
+    {
+      header: "Employee ID",
+      isDefault: true,
+      key: "employeeID",
+    },
+    {
+      header: "Name",
+      isDefault: true,
+      key: "name",
+    },
+    {
+      header: "Gross Wages",
+      isDefault: true,
+      key: "grossWages",
+    },
+    {
+      header: "Allowances",
+      isDefault: true,
+      key: "allowances",
+    },
+    {
+      header: "Expense Reimb.",
+      isDefault: true,
+      key: "expenseReimb",
+    },
+    {
+      header: "Employer Liability",
+      isDefault: true,
+      key: "employerLiability",
+    },
+    {
+      header: "Country VAT",
+      isDefault: true,
+      key: "countryVAT",
+    },
+    {
+      header: "Admin Fees",
+      isDefault: true,
+      key: "adminFees",
+    },
+    {
+      header: "Healthcare Benefits",
+      isDefault: true,
+      key: "healthcareBenefits",
+    },
+  ],
+  showDefaultColumn: true,
+  data: [
+    {
+      employeeID: "73917",
+      name: {
+        value: "Marcos Rodriguez",
+        img: { src: user },
+      },
+      grossWages: "EUR 32,000.00",
+      allowances: "EUR 0.00",
+      expenseReimb: "EUR 0.00",
+      employerLiability: "EUR 7,210.00",
+      countryVAT: "0.63",
+      adminFees: "USD 650.00",
+      healthcareBenefits: "USD 1260.00",
+    },
+    {
+      employeeID: "73917",
+      name: {
+        value: "Marcos Rodriguez",
+        img: { src: user },
+      },
+      grossWages: "EUR 32,000.00",
+      allowances: "EUR 0.00",
+      expenseReimb: "EUR 0.00",
+      employerLiability: "EUR 7,210.00",
+      countryVAT: "0.63",
+      adminFees: "USD 650.00",
+      healthcareBenefits: "USD 1260.00",
+    },
+    {
+      employeeID: "73917",
+      name: {
+        value: "Marcos Rodriguez",
+        img: { src: user },
+      },
+      grossWages: "EUR 32,000.00",
+      allowances: "EUR 0.00",
+      expenseReimb: "EUR 0.00",
+      employerLiability: "EUR 7,210.00",
+      countryVAT: "0.63",
+      adminFees: "USD 650.00",
+      healthcareBenefits: "USD 1260.00",
+    },
+    {
+      employeeID: "73917",
+      name: {
+        value: "Marcos Rodriguez",
+        img: { src: user },
+      },
+      grossWages: "EUR 32,000.00",
+      allowances: "EUR 0.00",
+      expenseReimb: "EUR 0.00",
+      employerLiability: "EUR 7,210.00",
+      countryVAT: "0.63",
+      adminFees: "USD 650.00",
+      healthcareBenefits: "USD 1260.00",
     },
   ],
 };
