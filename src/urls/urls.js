f0adedbfc76388d03cebba75a28dc7691a463b68
Ehsan <ehsan.shafique@techlogix.com>
2022-05-10 12:22:23 +0500

    pipeline enviornment setup

---
+++ src/urls/urls.js
@@ -0,0 +1,145 @@
+/* istanbul ignore next */
+const validateIsURLValid = () => {
+  if (
+    process?.env?.ATLAS_ENVIRONMENT &&
+    process?.env?.ATLAS_ENVIRONMENT !== "$ATLAS_ENVIRONMENT"
+  ) {
+    return process?.env?.ATLAS_ENVIRONMENT;
+  }
+  return "dev";
+};
+/* istanbul ignore next */
+
+/* istanbul ignore next */
+const validateIsRegionValid = () => {
+  if (
+    process?.env?.ATLAS_REGION &&
+    process?.env?.ATLAS_REGION !== "$ATLAS_REGION"
+  ) {
+    return process?.env?.ATLAS_REGION;
+  }
+  return "eu";
+};
+/* istanbul ignore next */
+
+const baseURL = `https://apigw-${validateIsURLValid()}-${validateIsRegionValid()}.atlasbyelements.com`;
+const metaDataUrl = `https://apigw-uat-emea.apnextgen.com`;
+
+const services = {
+  atlasInvoiceService: "/atlas-invoiceservice/api",
+  atlasIdgService: "/atlas-idg-service/api",
+  apngMetaDataService: "/metadataservice/api",
+  atlasCustomerService: "/cs/api",
+};
+
+export const urls = {
+  invoiceDetails:
+    baseURL + services.atlasIdgService + "/InvoiceData/GetPayrollForInvoice/",
+  countries:
+    metaDataUrl +
+    +services.apngMetaDataService +
+    `/lookup/Countries?includeProperties=Currency&orderBy=Name`,
+
+  fee: metaDataUrl + services.apngMetaDataService + `/Fees`,
+  lookup: metaDataUrl + services.apngMetaDataService + `/Lookup`,
+  voidUploadFile:
+    metaDataUrl + services.apngMetaDataService + "/Blob/UploadFile",
+  voidCreateDoc:
+    metaDataUrl + services.apngMetaDataService + "/InvoiceDocument/Create",
+  voidInvoice: baseURL + services.atlasInvoiceService + "/Invoices/voidInvoice",
+  saveNote: baseURL + services.atlasInvoiceService + `/InvoiceNote/Create`,
+
+  deleteFile:
+    baseURL + services.atlasInvoiceService + "/InvoiceDocument/Delete",
+  uploadFile: metaDataUrl + services.apngMetaDataService + "/Blob/UploadFile",
+  createDocument:
+    baseURL + services.atlasInvoiceService + "/InvoiceDocument/Create",
+
+  declineInvoice: baseURL + services + "/Invoices/declineInvoice",
+};
+
+export const getClientListingUrl = (
+  transactionTypes,
+  statusType,
+  dateFrom,
+  dateTo
+) => {
+  return (
+    baseURL +
+    services.atlasInvoiceService +
+    `/invoices/customer/filter?page=1&pageSize=10000&transactionTypes=${transactionTypes}&statuses=${statusType}&dateFrom=${dateFrom}&dateTo=${dateTo}`
+  );
+};
+
+export const getInternalListingUrl = (
+  transactionTypes,
+  statusType,
+  dateFrom,
+  dateTo
+) => {
+  return (
+    baseURL +
+    services.atlasInvoiceService +
+    `/invoices/filter?page=1&pageSize=10000&transactionTypes=${transactionTypes}&statuses=${statusType}&dateFrom=${dateFrom}&dateTo=${dateTo}`
+  );
+};
+
+export const getBillingAddressUrl = (cid) => {
+  return (
+    baseURL + services.atlasCustomerService + `/Customer?customerId=${cid}`
+  );
+};
+
+export const getNotesUrl = (id) => {
+  return baseURL + services.atlasInvoiceService + `/InvoiceNote/notes/${id}`;
+};
+
+export const getHeaders = (token, cid, isClient) => {
+  return {
+    authorization: `Bearer ${token}`,
+    "x-apng-base-region": "EMEA",
+    "x-apng-customer-id": cid || "",
+    "x-apng-external": isClient,
+    "x-apng-inter-region": "0",
+    "x-apng-target-region": "EMEA",
+    customer_id: cid || "",
+  };
+};
+
+export const getDownloadUrl = (id) => {
+  return baseURL + services.atlasInvoiceService + `/invoices/generatePDF/${id}`;
+};
+
+export const getApproveUrl = (id) => {
+  return baseURL + services.atlasInvoiceService + `/invoices/${id}/4`;
+};
+
+export const getApproveARUrl = () => {
+  return baseURL + services.atlasInvoiceService + `/Invoices/Reviewed`;
+};
+
+export const getExcelUrl = (id) => {
+  return (
+    baseURL + services.atlasInvoiceService + `/invoices/generateExcel/${id}`
+  );
+};
+
+export const getDeleteInvoiceUrl = (invoiceId) => {
+  return baseURL + services.atlasInvoiceService + `/Invoices/${invoiceId}`;
+};
+
+export const getAutoApproveCheckUrl = (id, isChecked) => {
+  return (
+    baseURL +
+    services.atlasInvoiceService +
+    `/Invoices/SaveInvoiceSetting/?invoiceId=${id}&settingTypeId=1&IsActive=${isChecked}`
+  );
+};
+
+export const getDownloadFileUrl = (docurl) => {
+  return (
+    metaDataUrl +
+    services.apngMetaDataService +
+    `/Blob/getBlobUrlWithSASToken?url=${docurl}`
+  );
+};
