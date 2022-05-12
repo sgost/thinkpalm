ff70e12612af7ecb97b7109d80f54d69ab24c352
Ehsan <ehsan.shafique@techlogix.com>
2022-05-12 16:05:13 +0500

    initial roles and permissions setup

--- src/layouts/views/InvoiceListing/index.tsx
+++ src/layouts/views/InvoiceListing/index.tsx
@@ -17,10 +17,12 @@ import {
   getInternalListingUrl,
 } from "../../../urls/urls";
 import { tableSharedColumns } from "../../../sharedColumns/sharedColumns";
+import { getDecodedToken } from "../../..//components/getDecodedToken";
 
 export default function InvoiceListing() {
   let navigate = useNavigate();
-  const accessToken = localStorage.getItem("accessToken")
+  const accessToken = localStorage.getItem("accessToken");
+  const permission: any = getDecodedToken();
   // internal token
   // const accessToken = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJxa1VoLVl2LWc3c25Zc3ktN1ktZVk0OE5TLTlzdldjWm9aMXFoMzZoYnpjIn0.eyJleHAiOjE2NTE5OTU0ODIsImlhdCI6MTY1MTgzNjc0NSwiYXV0aF90aW1lIjoxNjUxODIyNjgyLCJqdGkiOiJmNTE3MjM4NS00MzVkLTQzMjQtODA3My1lZmU4YWJlMTY4ZGYiLCJpc3MiOiJodHRwczovL2FjY291bnRzLXVhdC5hcG5leHRnZW4uY29tL2F1dGgvcmVhbG1zL2VsZW1lbnRzZ3MiLCJzdWIiOiI3NWM2MmFiNi1iMWMyLTQ5NTEtYmY2Yy00MjA3ZGQwZmEwNmEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhbmd1bGFyLXdlYi1jbGllbnQiLCJub25jZSI6IjBjMmYxNWY3LTg4YTItNDJjMS1hMDg0LWQ1ZDcxMWJkNjg1ZiIsInNlc3Npb25fc3RhdGUiOiIxNWU2MDRhMy01NDRhLTQ2YzYtYjYxOC1iZmU3MGZlZWIxYTQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vd3d3LXVhdC5hcG5leHRnZW4uY29tIiwiaHR0cHM6Ly9lbGVtZW50c2dzLW5nLmFwbmV4dGdlbi5jb20iLCJodHRwczovL2VsZW1lbnRzZ3MtdWF0LmFwbmV4dGdlbi5jb20iXSwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsInJvbGUiOiJBcHByb3ZQYXlPd25lcnMiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImlzRXh0ZXJuYWwiOmZhbHNlLCJuYW1lIjoiSmFzbWluZSBLYXVyIiwiaWQiOiI3NWM2MmFiNi1iMWMyLTQ5NTEtYmY2Yy00MjA3ZGQwZmEwNmEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJqYXNtaW5la0BlbGVtZW50c2dzLmNvbSIsImdpdmVuX25hbWUiOiJKYXNtaW5lIiwiZmFtaWx5X25hbWUiOiJLYXVyIiwiZW1haWwiOiJqYXNtaW5la0BlbGVtZW50c2dzLmNvbSJ9.Bz3I8XKE6cpPV_KdObEVBj1f3mAcQzXOnwKZzGblhJqf9HMB-QmNXilwL3rNkTioW-Oh2kbHwqj-V91OlFKaeL8JTW8KxEA2pxTxQtyXLYsaQzqRVZunVigTzJ4id2qRL4j8dpZhHFGgnmTXEX6com4alL-RCrBhHDACtOc-OVyi9Qr0zgU0E6q7vTyg1LAFhjUFcarp9YcVoquCCOSGEyflsbHiJVZ-PRoNtkz3psu6aGWYmsxfcoIo958pKSINjkmJjUiU-1C6_FgELQjStw88bj_trHAq-t2m6oGDOeaLopCsVpQLJatuGG6gMrmcbvmOCjtYgiDx51buFxppsA"
   const currentRoles = JSON.parse(localStorage.getItem("current-org") || "");
@@ -280,13 +282,13 @@ export default function InvoiceListing() {
   };
 
   useEffect(() => {
-    if(currentRoles?.Payments?.Role === 'Customer') {
-      setIsClient(true)
+    if (currentRoles?.Payments?.Role === "Customer") {
+      setIsClient(true);
     } else {
-      setIsClient(false)
+      setIsClient(false);
     }
-  }, [currentRoles])
-  
+  }, [currentRoles]);
+
   useEffect(() => {
     if (apiData?.data?.results) {
       const apiTableData: any = [];
@@ -389,8 +391,7 @@ export default function InvoiceListing() {
   }, [searchText]);
 
   useEffect(() => {
-    let tok = localStorage.getItem("current-org");
-    console.log("sakdj;kasjdlksajdkasjkldjaskljdklasj", tok);
+    console.log("sajkdh", permission);
   }, []);
 
   const downloadFunction = () => {
