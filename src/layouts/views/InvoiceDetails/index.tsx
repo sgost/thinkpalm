21b605d58aad06e59ad808847249ca52b5e751f9
Ehsan <ehsans.contractor@elementsgs.com>
2022-05-23 19:41:17 +0500

    url fix

--- src/layouts/views/InvoiceDetails/index.tsx
+++ src/layouts/views/InvoiceDetails/index.tsx
@@ -37,6 +37,7 @@ import {
   getDownloadFileUrl,
   getCMInvoiceUrl,
   getVatValue,
+  getAutoApproveCheckUrl,
 } from "../../../urls/urls";
 import CreditMemoSummary from "../CreditMemoSummary";
 import { tableSharedColumns } from "../../../sharedColumns/sharedColumns";
@@ -1169,20 +1170,21 @@ export default function InvoiceDetails() {
                       onChange={(e: any) => {
                         setIsAutoApprove(e.target.checked);
 
-                        const headers = {
-                          authorization: `Bearer ${tempToken}`,
-                          "x-apng-base-region": "EMEA",
-                          "x-apng-customer-id": cid || "",
-                          "x-apng-external": "false",
-                          "x-apng-inter-region": "0",
-                          "x-apng-target-region": "EMEA",
-                          customer_id: cid || "",
-                        };
+                        // const headers = {
+                        //   authorization: `Bearer ${tempToken}`,
+                        //   "x-apng-base-region": "EMEA",
+                        //   "x-apng-customer-id": cid || "",
+                        //   "x-apng-external": "false",
+                        //   "x-apng-inter-region": "0",
+                        //   "x-apng-target-region": "EMEA",
+                        //   customer_id: cid || "",
+                        // };
 
                         axios({
-                          url: `https://apigw-dev-eu.atlasbyelements.com/atlas-invoiceservice/api/Invoices/SaveInvoiceSetting/?invoiceId=${id}&settingTypeId=1&IsActive=${e.target.checked}`,
+                          // url: `https://apigw-dev-eu.atlasbyelements.com/atlas-invoiceservice/api/Invoices/SaveInvoiceSetting/?invoiceId=${id}&settingTypeId=1&IsActive=${e.target.checked}`,
+                          url: getAutoApproveCheckUrl(id, e.target.checked),
                           method: "POST",
-                          headers,
+                          headers: getHeaders(tempToken, cid, isClient),
                         })
                           .then((res: any) => {
                             if (res.status === 200) {
