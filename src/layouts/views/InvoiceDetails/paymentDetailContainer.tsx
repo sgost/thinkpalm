4fa4bd8b47cf318996b1864e91a453359cd80c21
smritis.contractor <smritis.contractor@elementsgs.com>
2022-06-09 19:25:24 +0530

    page

---
+++ src/layouts/views/InvoiceDetails/paymentDetailContainer.tsx
@@ -0,0 +1,381 @@
+import React, { useState } from "react";
+import { Button, DatePicker, Dropdown, Icon } from "atlasuikit";
+/* istanbul ignore next */
+const PaymentDetailContainer = () => {
+  const dropdownOptions = [
+    {
+      isSelected: false,
+      label: "Chocolate",
+      value: "chocolate",
+    },
+    {
+      isSelected: false,
+      label: "Strawberry",
+      value: "strawberry",
+    },
+    {
+      isSelected: false,
+      label: "Vanilla",
+      value: "vanilla",
+    },
+  ];
+
+  const [currencyOpen, setCurrencyOpen] = useState(false);
+  const [locationOpen, setLocationOpen] = useState(false);
+  const [classOpen, setClassOpen] = useState(false);
+  const [depositBankOpen, setDepositBankOpen] = useState(false);
+  const [editChecked, setEditChecked] = useState<any>();
+  const [addPaymentSectionCheck, setAddPaymentSectionCheck] = useState(false);
+  const [multiDetailPaymentBlocks, setMultiDetailPaymentBlocks] = useState([
+    {
+      id: Math.random(),
+      paymentDate: "",
+      currency: "",
+      location: "",
+      referenceNo: "",
+      depositedBank: "",
+      paymentMethod: "",
+      amount: "",
+    },
+  ]);
+
+  const addPaymentInstallmentBlocks = () => {
+    setAddPaymentSectionCheck(true);
+  };
+
+  return (
+    <div className="paymentDisplayContainer">
+      {multiDetailPaymentBlocks?.map((item: any, key: any) => {
+        return (
+          <div
+            className="paymentInstallmentContainer"
+          >
+            <div
+              className="paymentPageTitleHeader"
+            >
+              {key == 0 ? <p>Payment Details</p> : <></>}
+              <div className="topButtonActions">
+                {editChecked != key && (
+                  <div className="paymentDetailEdit">
+                    <Button
+                      className="primary-blue medium"
+                      icon={{
+                        color: "#fff",
+                        icon: "edit",
+                        size: "small",
+                      }}
+                      label="Edit"
+                      handleOnClick={() => {
+                        setEditChecked(key);
+                      }}
+                    />
+                  </div>
+                )}
+
+                {editChecked == key && (
+                  <div className="paymentPageCancelSave">
+                    <div className="paymentDetailCancel">
+                      <Button
+                        className="secondary-btn"
+                        label="Cancel Edit"
+                        handleOnClick={() => {
+                          setEditChecked(multiDetailPaymentBlocks.length);
+                        }}
+                      />
+                    </div>
+                    <div className="paymentDetailSave">
+                      <Button
+                        className="primary-blue medium"
+                        label="Save Changes"
+                      />
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <div className="paymentInstallmentUpperBlock">
+              <div className="paymentInstallmentDatepicker">
+                <DatePicker
+                  label="Payment Date"
+                  disabled={editChecked != key}
+                  required
+                />
+              </div>
+
+              <div className="paymentInstallmentContainerDropdowns">
+                <Dropdown
+                  handleDropdownClick={(b: boolean) => {
+                    setCurrencyOpen(b);
+                    setLocationOpen(false);
+                    setClassOpen(false);
+                    setDepositBankOpen(false);
+                  }}
+                  isOpen={currencyOpen}
+                  options={dropdownOptions}
+                  isDisabled={editChecked != key}
+                  title="Currency"
+                />
+              </div>
+
+              <div className="paymentInstallmentContainerDropdowns">
+                <Dropdown
+                  handleDropdownClick={(b: boolean) => {
+                    setCurrencyOpen(false);
+                    setLocationOpen(b);
+                    setClassOpen(false);
+                    setDepositBankOpen(false);
+                  }}
+                  isOpen={locationOpen}
+                  options={dropdownOptions}
+                  isDisabled={editChecked != key}
+                  title="Location"
+                />
+              </div>
+
+              <div className="paymentInstallmentContainerDropdowns">
+                <Dropdown
+                  handleDropdownClick={(b: boolean) => {
+                    setCurrencyOpen(false);
+                    setLocationOpen(false);
+                    setClassOpen(b);
+                    setDepositBankOpen(false);
+                  }}
+                  isOpen={classOpen}
+                  options={dropdownOptions}
+                  isDisabled={editChecked != key}
+                  title="Reference No"
+                />
+              </div>
+            </div>
+
+            <div className="paymentInstallmentLowerBlock">
+              <div className="paymentInnerLowerBlock">
+                <div className="paymentInstallmentContainerDropdowns">
+                  <Dropdown
+                    handleDropdownClick={(b: boolean) => {
+                      setCurrencyOpen(false);
+                      setLocationOpen(false);
+                      setClassOpen(false);
+                      setDepositBankOpen(b);
+                    }}
+                    isOpen={depositBankOpen}
+                    options={dropdownOptions}
+                    isDisabled={editChecked != key}
+                    title="Deposited to bank"
+                  />
+                </div>
+
+                <div className="paymentInstallmentContainerDropdowns">
+                  <Dropdown
+                    handleDropdownClick={(b: boolean) => {
+                      setCurrencyOpen(false);
+                      setLocationOpen(false);
+                      setClassOpen(false);
+                      setDepositBankOpen(b);
+                    }}
+                    isOpen={depositBankOpen}
+                    options={dropdownOptions}
+                    isDisabled={editChecked != key}
+                    title="Payment Method"
+                  />
+                </div>
+
+                <div className="PaymentPageTotalAmountInput">
+                  <div className="amountPaymentPageInput">
+                    <span>Amount</span>
+                    <input
+                      value="USD 300,523.15"
+                      type="number"
+                      className="disable-input-color"
+                      placeholder="Please enter"
+                      disabled={true}
+                      // disabled={disable}
+                      // onChange={(e)=>{setValue(e.target.value)}}
+                      // onKeyPress={(e)=>{masking(e)}}
+                    />
+                  </div>
+                  <div className="fullAmountPaymentNoInput">
+                    Payment #765248
+                  </div>
+                </div>
+              </div>
+
+              <div className="PaymentPageTotalAmount">
+                <p>Amount</p>
+                <div className="amountPaymentPage">USD 300,523.15</div>
+                <div className="fullAmountPaymentNo">Payment #765248</div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+
+      {addPaymentSectionCheck && (
+        <div
+        className="paymentInstallmentContainer border-line"
+      >
+        <div
+          className="paymentPageTitleHeaderNoTitle"
+        >
+          <div className="topButtonActions">
+            {addPaymentSectionCheck && (
+              <div className="paymentPageCancelSave">
+                <div className="paymentDetailCancel">
+                  <Button
+                    className="secondary-btn"
+                    label="Cancel"
+                    handleOnClick={() => {
+                      setAddPaymentSectionCheck(false);
+                    }}
+                  />
+                </div>
+                <div className="paymentDetailSave">
+                  <Button className="primary-blue medium" label="Save" />
+                </div>
+              </div>
+            )}
+            
+          </div>
+        </div>
+
+        <div className="paymentInstallmentUpperBlock">
+          <div className="paymentInstallmentDatepicker">
+            <DatePicker
+              label="Payment Date"
+              disabled={false}
+              required
+            />
+          </div>
+
+          <div className="paymentInstallmentContainerDropdowns">
+            <Dropdown
+              handleDropdownClick={(b: boolean) => {
+                setCurrencyOpen(b);
+                setLocationOpen(false);
+                setClassOpen(false);
+                setDepositBankOpen(false);
+              }}
+              isOpen={currencyOpen}
+              options={dropdownOptions}
+              isDisabled={false}
+              title="Currency"
+            />
+          </div>
+
+          <div className="paymentInstallmentContainerDropdowns">
+            <Dropdown
+              handleDropdownClick={(b: boolean) => {
+                setCurrencyOpen(false);
+                setLocationOpen(b);
+                setClassOpen(false);
+                setDepositBankOpen(false);
+              }}
+              isOpen={locationOpen}
+              options={dropdownOptions}
+              isDisabled={false}
+              title="Location"
+            />
+          </div>
+
+          <div className="paymentInstallmentContainerDropdowns">
+            <Dropdown
+              handleDropdownClick={(b: boolean) => {
+                setCurrencyOpen(false);
+                setLocationOpen(false);
+                setClassOpen(b);
+                setDepositBankOpen(false);
+              }}
+              isOpen={classOpen}
+              options={dropdownOptions}
+              isDisabled={false}
+              title="Reference No"
+            />
+          </div>
+        </div>
+
+        <div className="paymentInstallmentLowerBlock">
+          <div className="paymentInnerLowerBlock">
+            <div className="paymentInstallmentContainerDropdowns">
+              <Dropdown
+                handleDropdownClick={(b: boolean) => {
+                  setCurrencyOpen(false);
+                  setLocationOpen(false);
+                  setClassOpen(false);
+                  setDepositBankOpen(b);
+                }}
+                isOpen={depositBankOpen}
+                options={dropdownOptions}
+                isDisabled={false}
+                title="Deposited to bank"
+              />
+            </div>
+
+            <div className="paymentInstallmentContainerDropdowns">
+              <Dropdown
+                handleDropdownClick={(b: boolean) => {
+                  setCurrencyOpen(false);
+                  setLocationOpen(false);
+                  setClassOpen(false);
+                  setDepositBankOpen(b);
+                }}
+                isOpen={depositBankOpen}
+                options={dropdownOptions}
+                isDisabled={false}
+                title="Payment Method"
+              />
+            </div>
+
+            <div className="PaymentPageTotalAmountInput">
+              <div className="amountPaymentPageInput">
+                <span>Amount</span>
+                <input
+                  value="USD 300,523.15"
+                  type="number"
+                  className="disable-input-color"
+                  placeholder="Please enter"
+                  disabled={true}
+                  // disabled={disable}
+                  // onChange={(e)=>{setValue(e.target.value)}}
+                  // onKeyPress={(e)=>{masking(e)}}
+                />
+              </div>
+              <div className="fullAmountPaymentNoInput">
+                Payment #765248
+              </div>
+            </div>
+          </div>
+
+          <div className="PaymentPageTotalAmount">
+            <p>Amount</p>
+            <div className="amountPaymentPage">USD 300,523.15</div>
+            <div className="fullAmountPaymentNo">Payment #765248</div>
+          </div>
+        </div>
+      </div>
+      )}
+      
+      <div className="addPaymentInstallmentButton">
+        <div
+          className="addPaymentInstallmentIcon"
+          onClick={() => addPaymentInstallmentBlocks()}
+          aria-disabled={addPaymentSectionCheck}
+        >
+          <span>
+            <Icon
+              icon="add"
+              size="small"
+              width="20"
+              height="20"
+              color="white"
+              style={{ margin: `0 4px 0 0` }}
+            />
+          </span>
+          Add payment Installment
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentDetailContainer;
