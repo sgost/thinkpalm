3ff74ee0088da591486a4691d34b92734b471e7a
smritis.contractor <smritis.contractor@elementsgs.com>
2022-06-21 18:08:00 +0530

    bugs

--- src/layouts/views/PaymentDetailPage/paymentDetailPage.tsx
+++ src/layouts/views/PaymentDetailPage/paymentDetailPage.tsx
@@ -429,8 +429,8 @@ const PaymentDetailPage = () => {
           const total = state?.state?.inveoicesData?.reduce(
             (a: any, b: any) => {
               return (
-                a +
-                parseFloat(b?.invoiceBalance?.split(" ")[1]?.replaceAll(",", ""))
+                a + b
+                // parseFloat(b?.invoiceBalance?.split(" ")[1]?.replaceAll(",", ""))
               );
             },
             0
@@ -1121,7 +1121,7 @@ const PaymentDetailPage = () => {
             </div>
             <div>
               <p>
-                {state?.state?.inveoicesData[0].invoiceBalance.split(" ")[0]}{" "}
+                {/* {state?.state?.inveoicesData[0]?.invoiceBalance?.split(" ")[0]}{" "} */}
                 {toCurrencyFormat(multiTotal)}
               </p>
             </div>
