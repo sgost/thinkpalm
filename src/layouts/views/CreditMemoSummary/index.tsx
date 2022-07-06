cf2b67df61cffd1e3ef10aa88840fbda996ca6f1
Sumit V <sumitv@elementsgs.com>
2022-07-06 11:37:05 +0530

    changes in firfox

--- src/layouts/views/CreditMemoSummary/index.tsx
+++ src/layouts/views/CreditMemoSummary/index.tsx
@@ -511,7 +511,7 @@ export default function CreditMemoSummary(props: any) {
                     search
                   />
                 </div>
-                <div className="UI-line-text-box">
+                <div className="UI-line-text-box description">
                   <Input
                     setValue={(value: any) => {
                       setEditDescription(index, value);
@@ -653,7 +653,7 @@ export default function CreditMemoSummary(props: any) {
                   title="Product Service"
                 />
               </div>
-              <div className="UI-line-text-box">
+              <div className="UI-line-text-box description">
                 <Input
                   setValue={setNewDescription}
                   value={newDescription}
