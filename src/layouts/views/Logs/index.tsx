a35fe78b76d3495ca3442fa3c8c147d75b960466
smritis.contractor <smritis.contractor@elementsgs.com>
2022-07-07 11:58:07 +0530

    edit icon removed from change view logs

--- src/layouts/views/Logs/index.tsx
+++ src/layouts/views/Logs/index.tsx
@@ -27,33 +27,12 @@ const LogsCompo = (props: any) => {
                 custom
                 isOpen={isLogsOpen}
                 data={changeLogs}
-                title={
-                    <>
-                        <Icon
-                            icon="edit"
-                            size="small"
-                            color="#526FD6"
-                            viewBox="-2 -1 24 24"
-                            style={{
-                                marginTop: "0",
-                                padding: "0",
-                            }}
-                        />{" "}
-                        View Change Log
-                    </>
-                }
+                title="View Change Log"
                 name="View-change-log"
                 handleUpDown={() => setIsLogsOpen(!isLogsOpen)}
                 actions={{
                     primary: {
                         label: "View More",
-                        icon: {
-                            icon: "edit",
-                            size: "small",
-                            color: "#526FD6",
-                            viewBox: "-2 -1 24 24",
-                        },
-
                         handleOnClick: () => {
                             if (dataAvailable) {
                                 const spliced = [...logsData].splice(
@@ -73,12 +52,6 @@ const LogsCompo = (props: any) => {
                     },
                     secondary: {
                         label: "View Less",
-                        icon: {
-                            icon: "edit",
-                            size: "small",
-                            color: "#526FD6",
-                            viewBox: "-2 -1 24 24",
-                        },
                         handleOnClick: () => {
                             const logs = [...changeLogs];
                             logs.splice(initail, limitFor);
